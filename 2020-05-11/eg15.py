da33bff3f512a82788e213d3ff733e95f2b54960
Maria61 <mariaLi61@outlook.com>
2020-05-11 15:45:44 +0800

    seleniumAPI——文件操作

---
+++ 2020-05-11/eg15.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+import os
+
+# 文件上传操作
+
+driver = webdriver.Chrome()
+file_path = 'file:///'+os.path.abspath('upload.html')
+driver.get(file_path)
+time.sleep(4)
+
+driver.find_element_by_tag_name("input").send_keys('E://learningcode//testscript//2020-05-11//modal.html')
+
+time.sleep(4)
+driver.quit()
