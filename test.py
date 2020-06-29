c52e44da4fe6367f0636e157273da598f5d7d004
Maria61 <mariaLi61@outlook.com>
2020-06-29 22:26:55 +0800

    test

---
+++ test.py
@@ -0,0 +1,35 @@
+import os
+import time
+import selenium.webdriver
+
+driver =
+
+
+public static void bubbleSrot(int[] arr){
+    for(int i = 0; i < arr.length; i++){
+        boolean flag = false;
+        for(int j = 0; j < arr.length - 1 - i; j++){
+            if(arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                flag = true;
+            }
+        }
+        if(flag == false){
+            break;
+        }
+    }
+}
+
+ps aux | grep 123
+head -n 10
+
+
+student: id,course,grade,
+ select id from student  group by id having(avg(grade) > 60)
+
+
+
+
+
