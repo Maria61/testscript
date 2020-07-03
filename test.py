97172f9a6439514a9357aa775b94d70a686c668c
Maria61 <mariaLi61@outlook.com>
2020-07-03 18:52:49 +0800

    test

--- test.py
+++ test.py
@@ -1,34 +1,33 @@
-import os
-import time
-import selenium.webdriver
-
-driver =
-
-
-public static void bubbleSrot(int[] arr){
-    for(int i = 0; i < arr.length; i++){
-        boolean flag = false;
-        for(int j = 0; j < arr.length - 1 - i; j++){
-            if(arr[j] > arr[j + 1]){
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                flag = true;
-            }
-        }
-        if(flag == false){
-            break;
-        }
-    }
-}
-
-ps aux | grep 123
-head -n 10
+# import os
+# import time
+# from selenium import webdriver
+# driver =
 
 
-student: id,course,grade,
- select id from student  group by id having(avg(grade) > 60)
-
+# public static void bubbleSort(int[] arr){
+#     for(int i = 0; i < arr.length; i++){
+#         boolean flag = false;
+#         for(int j = 0; j < arr.length - 1 - i; j++){
+#             if(arr[j] > arr[j + 1]){
+#                 int temp = arr[j];
+#                 arr[j] = arr[j + 1];
+#                 arr[j + 1] = temp;
+#                 flag = true;
+#             }
+#         }
+#         if(flag == false){
+#             break;
+#         }
+#     }
+# }
+#
+# ps aux | grep 123
+# head -n 10
+#
+#
+# student: id,course,grade,
+# select id from student  group by id having(avg(grade) > 60)
+#
 
 
 
