d5b68ddb083f177874732aedc6b0ec8b2bd3cf4c
Maria61 <mariaLi61@outlook.com>
2020-07-08 00:01:15 +0800

    复习

--- 2020-07-03/eg0703.py
+++ 2020-07-03/eg0703.py
@@ -5,7 +5,6 @@ import os
 driver = webdriver.Chrome()
 driver.get("https://www.baidu.com")
 
-
 driver.find_element_by_id("kw").send_keys("字节跳动")
 time.sleep(5)
 driver.find_element_by_id("su").click()
