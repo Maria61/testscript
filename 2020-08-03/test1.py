bcc6f9537cdfcf84126f1df167961568622de267
Maria61 <mariaLi61@outlook.com>
2020-08-03 23:36:03 +0800

    python基础

---
+++ 2020-08-03/test1.py
@@ -0,0 +1,19 @@
+# 注释：“:”后缩进的语句视为代码块；python大小写敏感
+
+# a = 100
+# if a >= 0:
+#     print(a)
+# else:
+#     print(-a)
+#
+# print('I\'m \"OK\"')
+#
+# print('I\'m OK')
+#
+# print(r'\\\t\\\ ')
+# print('T \t i \n m')
+
+print('''asdf
+jlkl
+... werwqre
+... d''')
