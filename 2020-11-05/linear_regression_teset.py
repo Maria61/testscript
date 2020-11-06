d19b197b8a84bcb2f56f4922343c92b0ddff72ab
Maria61 <mariaLi61@outlook.com>
2020-11-06 21:15:16 +0800

    线性回归最小二乘法实现

--- 2020-11-05/linear_regression_teset.py
+++
@@ -1,2 +0,0 @@
-#简单线性回归（最小二乘法）
-import numpy as np
---
+++ machine_learning_test/linear_regression_teset.py
@@ -0,0 +1,9 @@
+#简单线性回归（最小二乘法）
+import numpy as np
+import matplotlib.pyplot as plt
+
+# points = np.genfromtxt('data.csv',delimiter=',')
+# print(points)
+#
+a = np.arange(10)
+print(a)
